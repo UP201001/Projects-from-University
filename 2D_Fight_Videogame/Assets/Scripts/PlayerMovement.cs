c6783d8cd9a486ca393298425f2a784cda5c4cee
UP201001 <up201001@alumnos.upa.edu.mx>
2022-12-04 17:12:39 -0600

    Movements & Animations
    The project was edited to add movement script and also animated the character while is moving.

---
+++ 2D_Fight_Videogame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Animator animator;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            jump = true;
+            animator.SetBool("isJumping", true);
+        }
+    }
+
+    public void OnLanding(){
+        animator.SetBool("isJumping", false);
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+    }
+}
